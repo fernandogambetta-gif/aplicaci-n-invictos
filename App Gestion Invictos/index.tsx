8aeaea9771c357d87fad283ffec6ed0093bc240a
fernandogambetta-gif <fernando.gambetta@gmail.com>
2025-12-17 18:10:14 -0300

    Update index.tsx

--- App Gestion Invictos/index.tsx
+++ App Gestion Invictos/index.tsx
@@ -3,7 +3,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { ErrorBoundary } from './components/ErrorBoundary';
-import './index.css';
 
 
 const rootElement = document.getElementById('root');
